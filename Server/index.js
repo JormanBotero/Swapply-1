d31f0de9e7a1de8aaa8d2c513bf13ae2a34cb630
Jorman Botero <jorman.botero@correounivalle.edu.co>
2025-12-15 10:33:22 -0500

    cambios para permitir imagenes más pesadas cuando se edita un producto

--- Server/index.js
+++ Server/index.js
@@ -120,7 +120,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '15mb', extended: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
